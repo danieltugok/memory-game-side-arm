4cd0a4b3208dfc1ccf9c78f10b49b28d21d5c03f
Daniel Kogut <danieltugok@gmail.com>
2022-06-23 17:23:28 -0500

    cleaning boilerplate files

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1 class="text-3xl font-bold underline">
+    Memory Game <span>SideArm</span>
+  </h1>
 </template>
 
+<script setup>
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
