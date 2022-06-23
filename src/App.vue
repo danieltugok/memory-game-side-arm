88089722d42e088edf76a1dc8dd0140197c858a2
Daniel Kogut <danieltugok@gmail.com>
2022-06-23 18:40:08 -0500

    cardboard game structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
 <template>
-  <div class="flex items-center justify-center">
-    <h1 class="text-3xl font-bold underline">
+  <div class="my-20 max-w-screen-xl flex flex-col justify-center items-center">
+    <h1 class="text-3xl font-bold text-center">
       Memory Game <span>SideArm</span>
     </h1>
+
+    <section class="game-board grid grid-cols-4 gap-8 justify-center">
+      <Card 
+        v-for="(card, index) in cardList"
+        :key="`card-${index}`"
+        :value="card"
+      />
+    </section>
   </div>
 </template>
 
-<script setup></script>
+<script>
+import Card from './components/Card.vue';
 
-<style></style>
+export default {
+  name: 'App',
+  components: {
+    Card
+  },
+  setup(){
+    const cardList = [];
+
+    for (let i = 0; i < 16; i++) {
+      cardList.push(i);      
+    }
+
+    return {
+      cardList
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="border-2 border-orange-500 w-24 h-24">
+    {{value}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
