c10abca410393aa8a9ee59ccd598e94d3870cf02
Daniel Kogut <danieltugok@gmail.com>
2022-06-23 21:06:10 -0500

    card front & back, selected cards, gamestatus and remaining pairs

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,105 @@
 <template>
   <div class="my-20 max-w-screen-xl flex flex-col justify-center items-center">
-    <h1 class="text-3xl font-bold text-center">
+    <h1 class="text-3xl font-bold text-center mb-10">
       Memory Game <span>SideArm</span>
     </h1>
 
-    <section class="game-board grid grid-cols-4 gap-8 justify-center">
-      <Card 
+    <section class="game-board grid grid-cols-4 gap-8 justify-center lg:grid-cols-6">
+      <Card
         v-for="(card, index) in cardList"
         :key="`card-${index}`"
-        :value="card"
+        :value="card.value"
+        :visible="card.visible"
+        :indexCard="card.indexCard"
+        :matched="card.matched"
+        @card-selected="flip"
       />
     </section>
+    <h2>{{ cardsSelected }}</h2>
+    <p>{{ statusSelection }}</p>
+    <p>{{remainingPairs}}</p>
+    <p>{{GameStatus}}</p>
+    <button @click="shuffleCards">Shuffle cards</button>
   </div>
 </template>
 
 <script>
-import Card from './components/Card.vue';
+import { ref, watch, computed } from "vue";
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Card
+    Card,
   },
-  setup(){
-    const cardList = [];
+  setup() {
+    const cardList = ref([]);
+    const cardsSelected = ref([]);
+    const statusSelection = ref("");
 
-    for (let i = 0; i < 16; i++) {
-      cardList.push(i);      
+    for (let i = 0; i < 24; i++) {
+      cardList.value.push({
+        value: i,
+        visible: true,
+        indexCard: i,
+        matched: false
+      });
     }
+
+    const GameStatus = computed(() => {
+      return remainingPairs.value === 0 ? 'Player Wins' : `Remaning pairs: ${remainingPairs.value}`
+    })
+
+    const remainingPairs = computed(() => {
+      return cardList.value.filter(card => card.matched === false).length / 2;
+    })
+
+    const shuffleCards = () => {
+      cardList.value = cardList.value.sort(() => Math.random() - 0.5);
+    }
+
+    const flip = (payload) => {
+      cardList.value[payload.indexCard].visible = true;
+
+      cardsSelected.value[0]
+        ? (cardsSelected.value[1] = payload)
+        : (cardsSelected.value[0] = payload);
+    };
+
+    watch(
+      cardsSelected,
+      (currentValue) => {
+        if (currentValue.length === 2) {
+          const firstCard = currentValue[0];
+          const secondCard = currentValue[1];
+
+          if (firstCard.cardValue === secondCard.cardValue) {
+            statusSelection.value = "Match!";
+            cardList.value[firstCard.indexCard].matched = true;
+            cardList.value[secondCard.indexCard].matched = true;
+          } else {
+            statusSelection.value = "Opps! Not yet...";
+            cardList.value[firstCard.indexCard].visible = false;
+            cardList.value[secondCard.indexCard].visible = false;
+          }
+
+          cardsSelected.value.length = 0;
+        }
+      },
+      { deep: true }
+    );
 
     return {
-      cardList
-    }
-  }
-}
+      cardList,
+      flip,
+      cardsSelected,
+      statusSelection,
+      remainingPairs,
+      GameStatus,
+      shuffleCards
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,20 +1,59 @@
 <template>
-  <div class="border-2 border-orange-500 w-24 h-24">
-    {{value}}
+  <div
+    @click="cardSelected"
+    class="flip-card border-2 border-orange-500 w-24 h-24 relative text-center"
+  >
+  <div class="flip-card-inner">
+    <div
+      v-if="visible"
+      class="flip-card-front bg-red-600 text-white min-w-full min-h-full absolute"
+    >
+      {{ value }} - {{matched}}
+    </div>
+    <div
+      v-else
+      class="flip-card-back bg-green-600 text-white min-w-full min-h-full absolute"
+    >
+      Back
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   props: {
+    indexCard: {
+      type: Number,
+      required: true
+    },
     value: {
       type: Number,
       required: true
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    matched: {
+      type: Boolean,
+      default: false
     }
-  }
-}
+  },
+
+  setup({indexCard, value}, context) {
+    const cardSelected = () => {
+      context.emit("card-selected", {
+        indexCard,
+        cardValue: value
+      });
+    };
+
+    return {
+      cardSelected,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
